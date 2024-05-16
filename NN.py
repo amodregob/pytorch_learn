56a58a7f826bbbf2b2e66d531defe696e0e130ce
amodrego <132926200+amodregob@users.noreply.github.com>
2024-05-16 12:09:27 +0200

    First commit
    First commit

---
+++ NN.py
@@ -0,0 +1,14 @@
+'''
+Structured data -> XGBoost or random forest
+    Machine learning
+Unstructured data -> neural networks
+    Deep learning
+
+For unstructured data (images, voices audios...), we need to transform the data into numbers --> Numerical encoding
+
+
+
+FOR LATER -> CHECK REINFORCEMENT LEARNING
+'''
+
+import torch
